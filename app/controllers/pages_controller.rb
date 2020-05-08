0ba6683c2e3b69e8a0516407feff3a7e71c86e26
EC2 Default User <ec2-user@ip-172-31-27-20.us-east-2.compute.internal>
2020-05-08 10:07:33 +0000

    improve pages design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def jobs
+  end
+  
+  def contact
+  end
+  
+  def sendcv
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
+  
+  get "/jobs" => "pages#jobs"
+  get "/contact" => "pages#contact"
+  get "/send-cv" => "pages#sendcv"
 end
