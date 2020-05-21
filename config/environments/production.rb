18661aa106e71b278b25c795b71111fe79484a37
EC2 Default User <ec2-user@ip-172-31-27-20.us-east-2.compute.internal>
2020-05-21 09:28:05 +0000

    try some shit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   config.action_mailer.mailgun_settings = {
     :api_key => ENV["MAILGUN_API_KEY"],
     :domain => ENV["MAILGUN_DOMAIN"],
-    :api_host => "api.eu.mailgun.net"
+    :api_host => "api.mailgun.net"
   }
 end
