4d88fba0cf3f92ca55c1f5ff166686bd8cba06f2
EC2 Default User <ec2-user@ip-172-31-27-20.us-east-2.compute.internal>
2020-05-21 08:08:35 +0000

    try another config for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY'],
-    domain: 'https://weems-builder.herokuapp.com',
-    api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
+    :api_key => ENV["MAILGUN_API_KEY"],
+    :domain => ENV["MAILGUN_DOMAIN"],
+    :api_host => "api.eu.mailgun.net"
   }
 end
