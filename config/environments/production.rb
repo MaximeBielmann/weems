5a7b7e841f711ee5ed05eb18cb67899cb124ac16
EC2 Default User <ec2-user@ip-172-31-27-20.us-east-2.compute.internal>
2020-05-21 08:14:29 +0000

    try to remove the eu domain api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     :api_key => ENV["MAILGUN_API_KEY"],
-    :domain => ENV["MAILGUN_DOMAIN"],
-    :api_host => "api.eu.mailgun.net"
+    :domain => ENV["MAILGUN_DOMAIN"]
   }
 end
