203f34c567a268b8a322c97bd967c9c24f1c69bc
EC2 Default User <ec2-user@ip-172-31-27-20.us-east-2.compute.internal>
2020-05-21 08:48:10 +0000

    change private key with public key mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    :api_key => ENV["MAILGUN_PUBLIC_KEY"],
+    :api_key => ENV["MAILGUN_API_KEY"],
     :domain => ENV["MAILGUN_DOMAIN"]
   }
 end
