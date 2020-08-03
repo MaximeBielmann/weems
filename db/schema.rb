3125ab652c7f2c31f8de5f6076ff58a9eb7f819e
MaximeBielmann <bielmann.maxime@gmail.com>
2020-08-03 09:47:56 +0000

    fix shit with ckeditor

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_090555) do
+ActiveRecord::Schema.define(version: 2020_08_03_094634) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_title"
-  end
-
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string "data_file_name", null: false
-    t.string "data_content_type"
-    t.integer "data_file_size"
-    t.string "data_fingerprint"
-    t.string "type", limit: 30
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "countries", force: :cascade do |t|
